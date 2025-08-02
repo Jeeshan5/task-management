f05afed88c94805caf4901635ba7728ef545652f
Jeeshan5 <mo.jeeshan1133@gmail.com>
2025-08-02 11:14:47 +0530

    MongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,16 @@ app.use(cors(
 ));
 app.use(express.json());
 
+//Connect to MongoDB
+const connectDB = require('./config/db');
+connectDB();
 
+
+//routes
 // app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportRoutes);
 
 //Start Server 
 const PORT = process.env.PORT || 5000;
